1cdfaaa76cf0ee291fc6feacefef806c19d27231
Alexandr <hav28081979@gmail.com>
2023-08-31 14:31:06 +0500

    Homework

---
+++ menu/task.js
@@ -0,0 +1,21 @@
+const menuLink = Array.from(document.getElementsByClassName('menu__link'));
+console.log(menuLink)
+
+menuLink.forEach(el => {
+    el.onclick = function () {
+        let parent = el.parentElement.querySelector('ul');
+        let active = el.closest('.menu_main').querySelector('.menu_active');
+
+        if (parent.classList.contains('menu_active')) {
+            active.classList.remove('menu_active');
+        } else {
+            if(active) {
+                active.classList.remove('menu_active');
+                parent.classList.add('menu_active')
+            } else {
+                parent.classList.add('menu_active')
+            }
+        }
+        return false;
+    };
+})
